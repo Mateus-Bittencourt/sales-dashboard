f9048f6b6b7e5ec8f06ae594ffb0dbf5cb067db2
Mateus <mateusmbittencourt@gmail.com>
2025-07-16 21:48:39 -0300

    feat: add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, GlobalStyle, lightTheme } from '@/styles'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
